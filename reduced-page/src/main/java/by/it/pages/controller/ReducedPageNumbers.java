7c6b8ff474f1e33e378740725394e0a668c01c86
tanyalivanovich <tanya1potapova@gmail.com>
2019-12-10 14:03:09 +0300

    exception

--- reduced-page/src/main/java/by/it/pages/controller/ReducedPageNumbers.java
+++ reduced-page/src/main/java/by/it/pages/controller/ReducedPageNumbers.java
@@ -13,23 +13,26 @@ import by.it.pages.number_pages.ReducedPageNumber;
 
 public class ReducedPageNumbers extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    public static final String ERROR = "Error: "; 
-    public ReducedPageNumbers() {
-        super();
-    }
+	public static final String ERROR = "Error: ";
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ReducedPageNumbers() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String str = request.getParameter(Names.PARAM);
 		PrintWriter out = response.getWriter();
 		try {
-		String result = ReducedPageNumber.reducedPageNumbers(str);
-		out.println(result);
+			String result = ReducedPageNumber.reducedPageNumbers(str);
+			out.println(result);
 		} catch (NumberFormatException e) {
-			response.sendError(response.SC_INTERNAL_SERVER_ERROR, ERROR+e.toString());
+			response.sendError(response.SC_INTERNAL_SERVER_ERROR, ERROR + e.toString());
 		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 }
